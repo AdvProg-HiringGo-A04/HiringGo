2b4273ec57352c15f1d726b63793008ce77333de
RahardiSalim <rahardi.salim@ui.ac.id>
2025-04-11 19:39:17 +0700

    test: add LogService interface

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/periksalog/service/LogService.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/periksalog/service/LogService.java
@@ -1,4 +1,11 @@
 package id.ac.ui.cs.advprog.hiringgo.periksalog.service;
 
-public class LogService {
+import id.ac.ui.cs.advprog.hiringgo.periksalog.dto.LogDTO;
+import id.ac.ui.cs.advprog.hiringgo.periksalog.dto.LogStatusUpdateDTO;
+import java.util.List;
+
+public interface LogService {
+    List<LogDTO> getAllLogsByDosenId(Long dosenId);
+    LogDTO updateLogStatus(Long dosenId, LogStatusUpdateDTO logStatusUpdateDTO);
+    boolean isLogOwnedByDosen(Long logId, Long dosenId);
 }
