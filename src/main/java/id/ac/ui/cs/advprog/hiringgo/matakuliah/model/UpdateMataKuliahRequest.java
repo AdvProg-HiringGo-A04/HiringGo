e213b9bb8dbddf146378f9e5c8795b08837f03ab
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-04-11 16:15:09 +0700

    [GREEN] Implementasi UpdateMataKuliahRequest

---
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/matakuliah/model/UpdateMataKuliahRequest.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.advprog.hiringgo.matakuliah.model;
+
+import id.ac.ui.cs.advprog.hiringgo.matakuliah.entity.Dosen;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateMataKuliahRequest {
+
+    @Size(max = 100)
+    private String namaMataKuliah;
+
+    @Size(max = 10)
+    private String kodeMataKuliah;
+
+    @NotBlank
+    private String deskripsiMataKuliah;
+
+    private List<Dosen> dosenPengampu;
+}
