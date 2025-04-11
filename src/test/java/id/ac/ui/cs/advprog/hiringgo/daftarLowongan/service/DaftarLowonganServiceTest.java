328afc0f843fe9c7293580d99331a9b0c4e7794d
narawaa <nashwa.ghania@ui.ac.id>
2025-04-11 18:26:07 +0700

    [RED] Create more tests

--- src/test/java/id/ac/ui/cs/advprog/hiringgo/daftarLowongan/service/DaftarLowonganServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/hiringgo/daftarLowongan/service/DaftarLowonganServiceTest.java
@@ -19,4 +19,42 @@ public class DaftarLowonganServiceTest {
 
         assertEquals("IPK harus antara 0 dan 4", exception.getMessage());
     }
+
+    @Test
+    void testFailedIfSKSNegative() {
+        DaftarLowonganCommand command = new DaftarLowonganCommand(-5, 3.0, 1L, 1001L);
+
+        DaftarLowonganService service = new DaftarLowonganService();
+
+        Exception exception = assertThrows(InvalidDataException.class, () -> {
+            service.execute(command);
+        });
+
+        assertEquals("SKS tidak boleh negatif", exception.getMessage());
+    }
+
+    @Test
+    void testSuccessForValidData() {
+        DaftarLowonganCommand command = new DaftarLowonganCommand(18, 3.25, 1L, 1001L);
+
+        DaftarLowonganService service = new DaftarLowonganService();
+
+        // sementara anggap ga throw exception artinya sukses
+        assertDoesNotThrow(() -> service.execute(command));
+    }
+
+    @Test
+    void testFailedIfIPKOver4() {
+        DaftarLowonganCommand command = new DaftarLowonganCommand(
+                20, 4.01, 1L, 1001L
+        );
+
+        DaftarLowonganService service = new DaftarLowonganService();
+
+        Exception exception = assertThrows(InvalidDataException.class, () -> {
+            service.execute(command);
+        });
+
+        assertEquals("IPK harus antara 0 dan 4", exception.getMessage());
+    }
 }
