8f6bad32852b89955a4f4b30e133a00b179f8133
RahardiSalim <rahardi.salim@ui.ac.id>
2025-04-11 20:48:25 +0700

    [Green] Implement DosenDashboardStrategy

---
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/dashboard/strategy/DosenDashboardStrategy.java
@@ -0,0 +1,20 @@
+// src/main/java/id/ac/ui/cs/advprog/hiringgo/dashboard/strategy/DosenDashboardStrategy.java
+package id.ac.ui.cs.advprog.hiringgo.dashboard.strategy;
+
+import id.ac.ui.cs.advprog.hiringgo.dashboard.dto.DosenStatisticsDTO;
+import id.ac.ui.cs.advprog.hiringgo.dashboard.repository.DashboardRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class DosenDashboardStrategy implements DashboardStatisticsStrategy<DosenStatisticsDTO> {
+    private final DashboardRepository dashboardRepository;
+
+    @Override
+    public DosenStatisticsDTO calculateStatistics(Long dosenId) {
+        return DosenStatisticsDTO.builder()
+                .totalMataKuliah(dashboardRepository.countMataKuliahByDosenId(dosenId))
+                .totalMahasiswaAssistant(dashboardRepository.countMahasiswaAssistantByDosenId(dosenId))
+                .openLowonganCount(dashboardRepository.countOpenLowonganByDosenId(dosenId))
+                .build();
+    }
+}
