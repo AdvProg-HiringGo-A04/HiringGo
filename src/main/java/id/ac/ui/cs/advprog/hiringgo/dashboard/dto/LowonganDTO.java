d028ed54d793340741146a1f69bf9541debffdac
RahardiSalim <rahardi.salim@ui.ac.id>
2025-04-11 20:38:38 +0700

    [Green] feat: implement LowonganDTO

---
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/dashboard/dto/LowonganDTO.java
@@ -0,0 +1,19 @@
+
+package id.ac.ui.cs.advprog.hiringgo.dashboard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LowonganDTO {
+    private Long id;
+    private String mataKuliahName;
+    private String mataKuliahCode;
+    private int tahunAjaran;
+    private String semester;
+}
