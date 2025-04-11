448e9fa74bba18507823a966c4b5448089edcc7b
RahardiSalim <rahardi.salim@ui.ac.id>
2025-04-11 20:56:43 +0700

    [Green] Feat: Create DashboardRepository Interface

---
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/repository/DashboardRepository.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.hiringgo.dashboard.repository;
+
+import id.ac.ui.cs.advprog.hiringgo.common.model.Lowongan;
+import java.util.List;
+
+public interface DashboardRepository {
+    // Admin stats
+    long countDosenUsers();
+    long countMahasiswaUsers();
+    long countMataKuliah();
+    long countLowongan();
+
+    // Dosen stats
+    long countMataKuliahByDosenId(Long dosenId);
+    long countMahasiswaAssistantByDosenId(Long dosenId);
+    long countOpenLowonganByDosenId(Long dosenId);
+
+    // Mahasiswa stats
+    long countOpenLowongan();
+    long countAcceptedLowonganByMahasiswaId(Long mahasiswaId);
+    long countRejectedLowonganByMahasiswaId(Long mahasiswaId);
+    long countPendingLowonganByMahasiswaId(Long mahasiswaId);
+    double calculateTotalLogHoursByMahasiswaId(Long mahasiswaId);
+    double calculateTotalInsentifByMahasiswaId(Long mahasiswaId);
+    List<Lowongan> findAcceptedLowonganByMahasiswaId(Long mahasiswaId);
+}
