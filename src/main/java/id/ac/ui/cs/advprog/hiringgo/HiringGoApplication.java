ca9ba95b1e4a2eae117ecbb90b6b123d200692c5
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-04-28 22:29:29 +0700

    Handle nullpointer

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/HiringGoApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/HiringGoApplication.java
@@ -1,18 +1,39 @@
 package id.ac.ui.cs.advprog.hiringgo;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class HiringGoApplication {
 
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        String datasourceUrl = dotenv.get("SPRING_DATASOURCE_URL");
+        String datasourceUsername = dotenv.get("SPRING_DATASOURCE_USERNAME");
+        String datasourcePassword = dotenv.get("SPRING_DATASOURCE_PASSWORD");
+
+        if (datasourceUrl != null) {
+            System.setProperty("SPRING_DATASOURCE_URL", datasourceUrl);
+        } else {
+            log.warn("SPRING_DATASOURCE_URL is missing in .env");
+        }
+
+        if (datasourceUsername != null) {
+            System.setProperty("SPRING_DATASOURCE_USERNAME", datasourceUsername);
+        } else {
+            log.warn("SPRING_DATASOURCE_USERNAME is missing in .env");
+        }
+
+        if (datasourcePassword != null) {
+            System.setProperty("SPRING_DATASOURCE_PASSWORD", datasourcePassword);
+        } else {
+            System.out.println("SPRING_DATASOURCE_PASSWORD is missing in .env");
+            log.warn("SPRING_DATASOURCE_PASSWORD is missing in .env");
+        }
 
         SpringApplication.run(HiringGoApplication.class, args);
     }
