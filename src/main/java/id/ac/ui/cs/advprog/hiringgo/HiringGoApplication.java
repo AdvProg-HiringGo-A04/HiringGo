08ad06175b6088aaefd7d1f132376d431c9218c2
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-04-28 23:01:18 +0700

    [REFACTOR] Move load env to new class

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/HiringGoApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/HiringGoApplication.java
@@ -1,40 +1,14 @@
 package id.ac.ui.cs.advprog.hiringgo;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class HiringGoApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-        String datasourceUrl = dotenv.get("SPRING_DATASOURCE_URL");
-        String datasourceUsername = dotenv.get("SPRING_DATASOURCE_USERNAME");
-        String datasourcePassword = dotenv.get("SPRING_DATASOURCE_PASSWORD");
-
-        if (datasourceUrl != null) {
-            System.setProperty("SPRING_DATASOURCE_URL", datasourceUrl);
-        } else {
-            log.warn("SPRING_DATASOURCE_URL is missing in .env");
-        }
-
-        if (datasourceUsername != null) {
-            System.setProperty("SPRING_DATASOURCE_USERNAME", datasourceUsername);
-        } else {
-            log.warn("SPRING_DATASOURCE_USERNAME is missing in .env");
-        }
-
-        if (datasourcePassword != null) {
-            System.setProperty("SPRING_DATASOURCE_PASSWORD", datasourcePassword);
-        } else {
-            log.warn("SPRING_DATASOURCE_PASSWORD is missing in .env");
-        }
-
+        EnvironmentConfig environmentConfig = new EnvironmentConfig();
+        environmentConfig.loadEnvVariables();
         SpringApplication.run(HiringGoApplication.class, args);
     }
-
 }
