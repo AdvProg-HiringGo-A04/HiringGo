8093d430c25f3cfc683f2b682443ae14f2cb288f
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-04-30 16:18:07 +0700

    [RED] Remove unused import

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/controller/AuthenticationController.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/controller/AuthenticationController.java
@@ -6,7 +6,6 @@ import id.ac.ui.cs.advprog.hiringgo.authentication.model.WebResponse;
 import id.ac.ui.cs.advprog.hiringgo.authentication.repository.UserRepository;
 import jakarta.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
