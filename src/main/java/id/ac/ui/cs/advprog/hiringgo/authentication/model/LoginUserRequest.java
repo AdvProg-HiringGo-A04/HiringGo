d0640fb1ebff4416fddd7b179c839ac16ab0e829
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-04-30 13:51:17 +0700

    [GREEN] Rename username to email

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/model/LoginUserRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/model/LoginUserRequest.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class LoginUserRequest {
 
     @NotBlank
-    private String username;
+    private String email;
 
     @NotBlank
     private String password;
