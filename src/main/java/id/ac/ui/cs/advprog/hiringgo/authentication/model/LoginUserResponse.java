86598a6d1f160d403d356ee7b4e2c10db8fe364c
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-04-30 13:31:54 +0700

    --amend

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/model/LoginUserResponse.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/model/LoginUserResponse.java
@@ -1,8 +1,10 @@
 package id.ac.ui.cs.advprog.hiringgo.authentication.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class LoginUserResponse {
 
     private String token;
