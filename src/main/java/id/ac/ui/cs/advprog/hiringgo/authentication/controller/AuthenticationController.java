ecf8947a46c61b46ab121716793be70eb9856c31
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-04-30 16:16:44 +0700

    [RED] Create Authentication Controller skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/controller/AuthenticationController.java
@@ -0,0 +1,42 @@
+package id.ac.ui.cs.advprog.hiringgo.authentication.controller;
+
+import id.ac.ui.cs.advprog.hiringgo.authentication.model.LoginUserRequest;
+import id.ac.ui.cs.advprog.hiringgo.authentication.model.LoginUserResponse;
+import id.ac.ui.cs.advprog.hiringgo.authentication.model.WebResponse;
+import id.ac.ui.cs.advprog.hiringgo.authentication.repository.UserRepository;
+import jakarta.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    Validator validator;
+
+    @PostMapping(
+            path = "/auth/login",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<WebResponse<LoginUserResponse>> login(@RequestBody LoginUserRequest request) {
+        // TODO: implement logic
+
+        LoginUserResponse loginUserResponse = new LoginUserResponse();
+        loginUserResponse.setToken("test"); // Ganti dengan JWT beneran
+
+        WebResponse<LoginUserResponse> response = WebResponse.<LoginUserResponse>builder()
+                .data(loginUserResponse)
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
+}
