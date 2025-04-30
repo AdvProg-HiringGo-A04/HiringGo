ebe652bfaf5c2019bca50e38b4a8e956bc527359
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-04-30 15:50:47 +0700

    [RED] Create JwtUtil skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/security/JwtUtil.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.hiringgo.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    public String extractEmail(String token) {
+    }
+
+    public String extractRole(String token) {
+    }
+}
