0df331e9eb3b34c27b8b3395e7ca0e49fb1a6c35
arishashaista <arishashaistaa@gmail.com>
2025-05-02 06:23:02 +0700

    [GREEN] Implement UserRepositoryImpl

---
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/manajemenakun/repository/UserRepositoryImpl.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.advprog.hiringgo.manajemenakun.repository;
+
+import id.ac.ui.cs.advprog.hiringgo.manajemenakun.model.Account;
+import java.util.*;
+
+public class UserRepositoryImpl implements UserRepository {
+    private final Map<String, Account> store = new HashMap<>();
+
+    @Override
+    public void save(Account account) {
+        store.put(account.getId(), account);
+    }
+
+    @Override
+    public Account findById(String id) {
+        return store.get(id);
+
+    }
+    @Override
+    public List<Account> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    @Override
+    public void delete(String id) {
+        store.remove(id);
+    }
+}
