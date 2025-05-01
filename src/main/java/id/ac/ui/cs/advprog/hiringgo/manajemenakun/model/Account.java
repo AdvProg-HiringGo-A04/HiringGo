1c0ef7a6c3b6b32537045f4992fd73e7c781c981
arishashaista <arishashaistaa@gmail.com>
2025-05-01 22:47:59 +0700

    [RED] Add Account.java

---
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/manajemenakun/model/Account.java
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.advprog.hiringgo.manajemenakun.model;
+
+public interface Account {
+    String getId();
+    String getEmail();
+    String getFullName();
+    Role getRole();
+}
