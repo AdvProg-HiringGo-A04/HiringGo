9534ea150e02ddddc273aca7f0d1fed6952cb97f
arishashaista <arishashaistaa@gmail.com>
2025-05-02 06:15:40 +0700

    [RED] Add new attribute for User Repository

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/manajemenakun/repository/UserRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/manajemenakun/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package id.ac.ui.cs.advprog.hiringgo.manajemenakun.repository;
 
-import id.ac.ui.cs.advprog.hiringgo.manajemenakun.model.User;
+import id.ac.ui.cs.advprog.hiringgo.manajemenakun.model.Account;
 
 import java.util.List;
 
 public interface UserRepository {
-    List<User> getAllUsers();
+    void save(Account account);
+    Account findById(String id);
+    List<Account> findAll();
+    void delete(String id);
 }
