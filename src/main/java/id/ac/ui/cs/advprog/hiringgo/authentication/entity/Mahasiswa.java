b0394bf7a2970fc45449144afcb4fb71d299dac0
RahardiSalim <rahardi.salim@ui.ac.id>
2025-05-02 11:09:38 +0700

    [Red] feat: implement entity/Mahasiswa

---
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/entity/Mahasiswa.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.hiringgo.authentication.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "mahasiswa")
+@Entity
+public class Mahasiswa {
+
+    @Id
+    private String id;
+
+    private String namaLengkap;
+
+    private String NPM;
+}
