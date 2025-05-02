65c0e46f62e3b414311abc9cb19e9d563618124d
arishashaista <arishashaistaa@gmail.com>
2025-05-02 12:34:23 +0700

    [GREEN] Fix Honor Calculator Implementation

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/dashboardhonor/service/HonorCalculatorImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/dashboardhonor/service/HonorCalculatorImpl.java
@@ -1,8 +1,11 @@
 package id.ac.ui.cs.advprog.hiringgo.dashboardhonor.service;
 
+import org.springframework.stereotype.Service;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Service
 public class HonorCalculatorImpl implements HonorCalculator {
     private static final BigDecimal RATE_PER_HOUR = new BigDecimal("27500");
 
