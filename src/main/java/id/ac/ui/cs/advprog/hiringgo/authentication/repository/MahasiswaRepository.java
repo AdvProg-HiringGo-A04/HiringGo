9e885c2a7c990263a22a1f3bb4e0bc98a71c4ce8
RahardiSalim <rahardi.salim@ui.ac.id>
2025-05-02 11:11:12 +0700

    [Red] feat: implement MahasiswaRepository

---
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/repository/MahasiswaRepository.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.hiringgo.authentication.repository;
+
+import id.ac.ui.cs.advprog.hiringgo.authentication.entity.Mahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MahasiswaRepository extends JpaRepository<Mahasiswa, String> {
+    Optional<Mahasiswa> findByNPM(String npm);
+}
