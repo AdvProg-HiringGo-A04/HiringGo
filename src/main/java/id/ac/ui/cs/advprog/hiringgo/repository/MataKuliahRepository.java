9750f23f8860c246f733e7ce4a37f08d43536e93
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-05-02 15:27:37 +0700

    [REFACTOR] Move mata kuliah repository

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/repository/MataKuliahRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/repository/MataKuliahRepository.java
@@ -1,4 +1,4 @@
-package id.ac.ui.cs.advprog.hiringgo.matakuliah.repository;
+package id.ac.ui.cs.advprog.hiringgo.repository;
 
 import id.ac.ui.cs.advprog.hiringgo.entity.MataKuliah;
 import org.springframework.data.jpa.repository.JpaRepository;
