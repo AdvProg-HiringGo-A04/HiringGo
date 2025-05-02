c71a5072b82422fde442f9b91ef21b4dff7397a8
arishashaista <arishashaistaa@gmail.com>
2025-05-02 11:08:01 +0700

    [RED] Add test for lowongan model

---
+++ src/test/java/id/ac/ui/cs/advprog/hiringgo/dashboardhonor/model/LowonganTest.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.hiringgo.dashboardhonor.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LowonganTest {
+    @Test
+    void testSetterAndGetter() {
+        Lowongan vac = new Lowongan();
+        vac.setId("csui-123");
+        vac.setStudentId("mhs-456");
+        assertEquals("csui-123", vac.getId());
+        assertEquals("mhs-456", vac.getStudentId());
+    }
+}
