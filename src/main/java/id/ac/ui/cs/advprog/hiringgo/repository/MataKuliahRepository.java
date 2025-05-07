ebb53829d756ba4c2b9a3ac7fba9e72c91ba2b6a
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-05-07 18:46:54 +0700

    [GREEN] Add new method findByKodeMataKuliah

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/repository/MataKuliahRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/repository/MataKuliahRepository.java
@@ -4,6 +4,10 @@ import id.ac.ui.cs.advprog.hiringgo.entity.MataKuliah;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MataKuliahRepository extends JpaRepository<MataKuliah, String> {
+
+    Optional<MataKuliah> findByKodeMataKuliah(String kode);
 }
