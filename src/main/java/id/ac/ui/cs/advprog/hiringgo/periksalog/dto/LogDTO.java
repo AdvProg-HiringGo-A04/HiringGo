6f350a7886c9c756521aeed86175f8c84f1d73b8
RahardiSalim <rahardi.salim@ui.ac.id>
2025-05-09 17:37:27 +0700

    [Green] refactor(core): Replaced common model references with actual domain implementations in LogDTO

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/periksalog/dto/LogDTO.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/periksalog/dto/LogDTO.java
@@ -1,7 +1,7 @@
 package id.ac.ui.cs.advprog.hiringgo.periksalog.dto;
 
-import id.ac.ui.cs.advprog.hiringgo.common.model.LogCategory;
-import id.ac.ui.cs.advprog.hiringgo.common.model.LogStatus;
+import id.ac.ui.cs.advprog.hiringgo.manajemenLog.model.enums.StatusLog;
+import id.ac.ui.cs.advprog.hiringgo.manajemenLog.model.enums.TipeKategori;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,15 +14,15 @@ import java.time.LocalTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LogDTO {
-    private Long id;
+    private String id;
     private String judul;
     private String keterangan;
-    private LogCategory kategori;
+    private TipeKategori kategori;
     private LocalTime waktuMulai;
     private LocalTime waktuSelesai;
     private LocalDate tanggalLog;
     private String pesanUntukDosen;
-    private LogStatus status;
+    private StatusLog status;
     private String mahasiswaName;
     private String mataKuliahName;
     private String mataKuliahCode;
