03a715b7fa3bfc840aaca5d057dadb5bd84bf7a7
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-05-21 19:37:47 +0700

    [RED] Modify delete mata kuliah test

--- src/test/java/id/ac/ui/cs/advprog/hiringgo/matakuliah/service/MataKuliahServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/hiringgo/matakuliah/service/MataKuliahServiceTest.java
@@ -172,22 +172,28 @@ public class MataKuliahServiceTest {
 
     @Test
     void testDeleteMataKuliahSuccess() {
+        Mockito.when(mataKuliahRepository.findById(mataKuliah1.getKodeMataKuliah()))
+                .thenReturn(Optional.of(mataKuliah1));
+
         doNothing().when(mataKuliahRepository).deleteById(mataKuliah1.getKodeMataKuliah());
 
         mataKuliahService.deleteMataKuliah(mataKuliah1.getKodeMataKuliah());
 
+        verify(mataKuliahRepository, times(1)).findById(mataKuliah1.getKodeMataKuliah());
         verify(mataKuliahRepository, times(1)).deleteById(mataKuliah1.getKodeMataKuliah());
     }
 
     @Test
     void testDeleteMataKuliahWhenKodeIsNotFound() {
-        doNothing().when(mataKuliahRepository).deleteById(mataKuliah1.getKodeMataKuliah());
+        Mockito.when(mataKuliahRepository.findById(mataKuliah1.getKodeMataKuliah()))
+                .thenReturn(Optional.empty());
 
         ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
             mataKuliahService.deleteMataKuliah(mataKuliah1.getKodeMataKuliah());
         });
 
         assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
-        verify(mataKuliahRepository, times(1)).deleteById(mataKuliah1.getKodeMataKuliah());
+        verify(mataKuliahRepository, times(1)).findById(mataKuliah1.getKodeMataKuliah());
+        verify(mataKuliahRepository, times(0)).deleteById(mataKuliah1.getKodeMataKuliah());
     }
 }
