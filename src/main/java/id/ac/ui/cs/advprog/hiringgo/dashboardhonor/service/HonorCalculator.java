0aab23016fb9ae329631477829ecbd6833d05e40
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-05-22 10:41:45 +0700

    [RED] Remove implementations and tests

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/dashboardhonor/service/HonorCalculator.java
+++
@@ -1,8 +0,0 @@
-package id.ac.ui.cs.advprog.hiringgo.dashboardhonor.service;
-
-import java.math.BigDecimal;
-
-public interface HonorCalculator {
-    BigDecimal calculate(BigDecimal totalHours);
-    BigDecimal getRatePerHour();
-}
