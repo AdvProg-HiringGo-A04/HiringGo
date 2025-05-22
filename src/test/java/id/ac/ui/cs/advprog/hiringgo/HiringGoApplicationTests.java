64afcd52fe924eacd04b974f006b26860dae6db9
arishashaista <arishashaistaa@gmail.com>
2025-05-22 07:00:54 +0700

    [GREEN] Add test for Authentication Controller

--- src/test/java/id/ac/ui/cs/advprog/hiringgo/HiringGoApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/hiringgo/HiringGoApplicationTests.java
@@ -3,12 +3,12 @@ package id.ac.ui.cs.advprog.hiringgo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import id.ac.ui.cs.advprog.hiringgo.manajemenakun.service.UserService;
+//import id.ac.ui.cs.advprog.hiringgo.manajemenakun.service.UserService;
 
 @SpringBootTest
 class HiringGoApplicationTests {
-    @MockitoBean
-    private UserService userService;
+//    @MockitoBean
+//    private UserService userService;
 
     @Test
     void contextLoads() {
