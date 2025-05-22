4688da9fa07bb33793736dbb20abf35c9df21a02
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-05-22 12:17:37 +0700

    [REFACTOR] Change data type

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/repository/LogRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/repository/LogRepository.java
@@ -3,12 +3,12 @@ package id.ac.ui.cs.advprog.hiringgo.repository;
 import id.ac.ui.cs.advprog.hiringgo.entity.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface LogRepository extends JpaRepository<Log, String> {
 
     List<Log> findByMahasiswaNPM(String NPM);
 
-    List<Log> findByTanggalLogBetweenAndMahasiswaNPM(Date startDate, Date endDate, String npm);
+    List<Log> findByTanggalLogBetweenAndMahasiswaNPM(LocalDate tanggalLog, LocalDate tanggalLog2, String mahasiswa_NPM);
 }
