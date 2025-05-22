ff332b0355fdc6bb98bac227facd309a4ab09323
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-05-22 11:20:23 +0700

    [REFACTOR] Add annotations

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/dashboardhonor/model/HonorResponse.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/dashboardhonor/model/HonorResponse.java
@@ -2,9 +2,17 @@ package id.ac.ui.cs.advprog.hiringgo.dashboardhonor.model;
 
 import id.ac.ui.cs.advprog.hiringgo.entity.Mahasiswa;
 import id.ac.ui.cs.advprog.hiringgo.entity.MataKuliah;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class HonorResponse {
 
     private Date tanggal;
