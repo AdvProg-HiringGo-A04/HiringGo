7b5bea52384f6c3864562065135853bde9c19b48
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-05-23 17:59:53 +0700

    [GREEN] Configure logging

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/controller/AuthenticationController.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/controller/AuthenticationController.java
@@ -12,6 +12,7 @@ import id.ac.ui.cs.advprog.hiringgo.security.JwtUtil;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -27,6 +28,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Set;
 
+@Slf4j
 @RestController
 public class AuthenticationController {
 
@@ -73,6 +75,8 @@ public class AuthenticationController {
 
         LoginUserResponse loginUserResponse = new LoginUserResponse();
         loginUserResponse.setToken(token);
+
+        log.info("Authentication successful for user: {}", user.getEmail());
 
         WebResponse<LoginUserResponse> response = WebResponse.<LoginUserResponse>builder()
                 .data(loginUserResponse)
@@ -128,6 +132,8 @@ public class AuthenticationController {
         mahasiswa.setNPM(request.getNPM());
         mahasiswaRepository.save(mahasiswa);
 
+        log.info("New user registered with email: {}", user.getEmail());
+
         WebResponse<String> response = WebResponse.<String>builder()
                 .data("Registration successful")
                 .build();
@@ -150,6 +156,8 @@ public class AuthenticationController {
         if (!jwtUtil.validateToken(token)) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
         }
+
+        log.info("User logged out: {}", jwtUtil.extractEmail(token));
 
         WebResponse<String> response = WebResponse.<String>builder()
                 .data("OK")
