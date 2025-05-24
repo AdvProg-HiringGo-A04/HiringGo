78ed4b7097059d3ea23787d52a1328b0fb252417
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-05-24 23:23:41 +0700

    [REFACTOR] Remove library dotenv-java

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,6 @@ repositories {
 
 dependencies {
     implementation ("org.springframework.boot:spring-boot-starter-jdbc")
-    implementation("io.github.cdimascio:dotenv-java:3.2.0")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
