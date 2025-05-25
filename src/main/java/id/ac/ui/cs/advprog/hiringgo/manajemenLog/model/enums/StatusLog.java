a56cb464efeab4c4133688a3fac86070c2f52fde
RahardiSalim <rahardi.salim@ui.ac.id>
2025-05-25 14:58:37 +0700

    [REFACTOR] Fixing test

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/manajemenLog/model/enums/StatusLog.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/manajemenLog/model/enums/StatusLog.java
@@ -1,4 +1,17 @@
 package id.ac.ui.cs.advprog.hiringgo.manajemenLog.model.enums;
 
-public class StatusLog {
+public enum StatusLog {
+    DIPROSES("Diproses"),
+    DITERIMA("Diterima"),
+    DITOLAK("Ditolak");
+
+    private final String displayName;
+
+    StatusLog(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
