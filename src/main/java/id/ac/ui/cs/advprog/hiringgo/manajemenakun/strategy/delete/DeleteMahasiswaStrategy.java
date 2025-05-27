76fbfa2b5500070013d5c28d5f7d393115cdfda0
arishashaista <arishashaistaa@gmail.com>
2025-05-27 09:29:04 +0700

    [REFACTOR] Refactoring async annotation in manajemen akun

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/manajemenakun/strategy/delete/DeleteMahasiswaStrategy.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/manajemenakun/strategy/delete/DeleteMahasiswaStrategy.java
@@ -4,7 +4,6 @@ import id.ac.ui.cs.advprog.hiringgo.enums.Role;
 import id.ac.ui.cs.advprog.hiringgo.repository.MahasiswaRepository;
 import id.ac.ui.cs.advprog.hiringgo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
